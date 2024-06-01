a7654d1af2e628d099d47b676545261c065a47eb
Emre <segorucu@gmail.com>
2024-06-01 00:09:17 +0000

    Code challenge submission for question: 3110 Score of a String

---
+++ 3110 Score of a String/3110score-of-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+
+        tot = 0
+        for i in range(len(s)-1):
+            tot += abs(ord(s[i])-ord(s[i+1]))
+
+        return tot
+        
