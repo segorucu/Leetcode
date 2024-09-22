7fdddef10c3327ca2702c417dd6364bcf10ed23e
Emre <segorucu@gmail.com>
2024-09-22 23:29:39 +0000

    Code challenge submission for question: 3298 Count Substrings That Can Be Rearranged to Contain a String II

---
+++ 3298 Count Substrings That Can Be Rearranged to Contain a String II/3298count-substrings-that-can-be-rearranged-to-contain-a-string-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validSubstringCount(self, word1: str, word2: str) -> int:
+        c2 = collections.Counter(word2)
+        c1 = collections.Counter()
+
+        l = 0
+        n = len(word1)
+        r = 0
+        ans = 0
+        completed = False
+        while r < n:
+            a = word1[r]
+            c1[a] += 1
+            for key, val in c2.items():
+                if c1[key] < val:
+                    break
+            else:
+                completed = True
+            if not completed:
+                r += 1
+                continue
+            left = word1[l]
+            while c1[left] > c2[left]:
+                l += 1
+                c1[left] -= 1
+                left = word1[l]
+            r += 1
+            ans += l+1
+
+        return ans
