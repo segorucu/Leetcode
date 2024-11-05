b07a6a1b58afa9ee2e518f97e8a4666fe4c222b1
Emre <segorucu@gmail.com>
2024-11-05 07:49:51 +0000

    Code challenge submission for question: 2914 Minimum Number of Changes to Make Binary String Beautiful

---
+++ 2914 Minimum Number of Changes to Make Binary String Beautiful/2914minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minChanges(self, s: str) -> int:
+        
+        ans = 0
+        n = len(s)
+        for i in range(1,n,2):
+            if s[i] != s[i-1]:
+                ans += 1
+
+
+        return ans
