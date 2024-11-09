12774414d092e4cfdec9abe81dcf20c045915403
Emre <segorucu@gmail.com>
2024-11-09 15:37:26 +0000

    Code challenge submission for question: 1603 Design Parking System

---
+++ 1603 Design Parking System/1603design-parking-system.py
@@ -0,0 +1,18 @@
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.space = [big, medium, small]
+        
+
+    def addCar(self, carType: int) -> bool:
+        carType -= 1
+        if self.space[carType]:
+            self.space[carType] -= 1
+            return True
+        return False
+        
+
+
+# Your ParkingSystem object will be instantiated and called as such:
+# obj = ParkingSystem(big, medium, small)
+# param_1 = obj.addCar(carType)
