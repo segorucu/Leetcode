5799840f3499e449cd995191b72b35490764bb45
segorucu <32857400+segorucu@users.noreply.github.com>
2025-01-30 19:28:37 -0700

    Time: 1436 ms (25.13%) | Memory: 66.7 MB (13.12%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -2,13 +2,12 @@ class Solution:
     def largestIsland(self, grid: List[List[int]]) -> int:
         
 
-        m = len(grid)
         n = len(grid[0])
 
         groups = collections.defaultdict(set)
 
         def valid(r,c):
-            return 0 <= r < m and 0 <= c < n 
+            return 0 <= r < n and 0 <= c < n 
 
         def dfs(r,c):
             neighs = [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
@@ -21,7 +20,7 @@ class Solution:
         visited = set()
         groupindex = collections.defaultdict(int)
         ans = 1
-        for r in range(m):
+        for r in range(n):
             for c in range(n):
                 if (r,c) not in visited and grid[r][c] == 1:
                     curr = set()
@@ -35,10 +34,10 @@ class Solution:
                     group += 1
 
         if len(groups.keys()) == 1:
-            return min(m*n,ans)
+            return min(n*n,ans)
 
 
-        for r in range(m):
+        for r in range(n):
             for c in range(n):
                 if grid[r][c] == 0:
                     neighs = [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
