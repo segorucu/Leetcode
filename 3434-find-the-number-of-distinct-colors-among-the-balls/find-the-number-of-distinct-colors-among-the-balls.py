cd397919b6dc82eb6b4faaefc9bd71a78d7f535d
segorucu <32857400+segorucu@users.noreply.github.com>
2025-02-06 18:19:44 -0700

    Time: 91 ms (31.62%) | Memory: 63.4 MB (31.85%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -4,6 +4,12 @@ class Solution:
         if counter[prev] == 0:
             del counter[prev]
 
+    def add(self, counter, col):
+        if col in counter:
+            counter[col] += 1
+        else:
+            counter[col] = 1
+
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
         
         colours = {}
@@ -14,19 +20,14 @@ class Solution:
         for i,q in enumerate(queries):
             ind, col = q[0], q[1]
             if ind not in colours:
-                if col in counter:
-                    counter[col] += 1
-                else:
-                    counter[col] = 1
+                self.add(counter, col)
             elif colours[ind] == col:
                 pass
             else:
                 prev = colours[ind]
                 self.remove(counter, prev)
-                if col in counter:
-                    counter[col] += 1
-                else:
-                    counter[col] = 1
+                self.add(counter, col)
+                
             ans[i] = len(counter.keys())
             colours[ind] = col
         return ans
