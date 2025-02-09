a53cd0eac5dd7a10ef1de709962c399ac991389f
segorucu <32857400+segorucu@users.noreply.github.com>
2025-02-09 14:48:42 -0700

    Time: 711 ms (71.24%) | Memory: 44.4 MB (76.92%) - LeetSync

---
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.py
@@ -0,0 +1,24 @@
+from bisect import bisect_left
+class Solution:
+    def minOperations(self, nums: List[int], queries: List[int]) -> List[int]:
+        
+        nums.sort()
+        prefix = []
+        curr = 0
+        for num in nums:
+            curr += num
+            prefix.append(curr)
+
+        n = len(nums)
+        ans = []
+        for q in queries:
+            ind = bisect_right(nums,q)
+            if ind == n or ind == 0:
+                cost = abs(n * q - prefix[-1])
+                ans.append(cost)
+            else:
+                cost = ind * q - prefix[ind-1]
+                cost += prefix[-1] - prefix[ind-1] - (n-ind) * q
+                ans.append(cost)
+
+        return ans
