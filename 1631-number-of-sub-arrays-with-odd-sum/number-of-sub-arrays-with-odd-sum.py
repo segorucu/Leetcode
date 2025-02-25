faca4655f08ea5718b274800321fca00c9a0d9a4
segorucu <32857400+segorucu@users.noreply.github.com>
2025-02-24 18:57:31 -0700

    Time: 302 ms (5.11%) | Memory: 37 MB (5.11%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -14,11 +14,11 @@ class Solution:
         tot = dp[0][1]
         for i in range(1,n):
             if arr[i] % 2 == 1:
-                dp[i][0] = dp[i-1][1]
+                dp[i][0] = dp[i-1][1] % MOD
                 dp[i][1] = dp[i-1][0] + 1
             else:
                 dp[i][0] = dp[i-1][0] + 1
-                dp[i][1] = dp[i-1][1]
+                dp[i][1] = dp[i-1][1] % MOD
             tot += dp[i][1] % MOD
 
         # print(dp)
