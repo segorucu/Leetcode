016b08c3b0ed6812fb0aff46cfbcda486e67f51d
segorucu <32857400+segorucu@users.noreply.github.com>
2025-02-24 18:57:16 -0700

    Time: 291 ms (5.11%) | Memory: 33.8 MB (5.11%) - LeetSync

--- 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/number-of-sub-arrays-with-odd-sum.py
@@ -19,7 +19,7 @@ class Solution:
             else:
                 dp[i][0] = dp[i-1][0] + 1
                 dp[i][1] = dp[i-1][1]
-            tot += dp[i][1]
+            tot += dp[i][1] % MOD
 
         # print(dp)
         return tot % MOD
