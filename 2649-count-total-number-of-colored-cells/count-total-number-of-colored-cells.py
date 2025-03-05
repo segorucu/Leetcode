72dd38fae7a4f5c63c264853b95eb8f0243f61c4
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-04 17:51:47 -0700

    Time: 155 ms (21.80%) | Memory: 21.9 MB (6.77%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -1,13 +1,5 @@
 class Solution:
     def coloredCells(self, n: int) -> int:
-        
-        0
-        1
-        4
-        8
-        12
-        16
-        20
 
         dp = (n+1) * [0]
         dp[1] = 1
