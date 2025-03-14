1eac79c117672bfe4c01cce01d29bdc647df4205
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-13 21:47:01 -0600

    Time: 129 ms (95.49%) | Memory: 29.8 MB (54.00%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.py
@@ -7,24 +7,20 @@ class Solution:
             tot = 0
             for candy in candies:
                 tot += candy // val
-            # print(tot, val)
             return tot >= k
 
-        # print(good(5))
 
         right = sum(candies) // k
-        left = 0
-        if right == 0:
-            return 0
-
-        while left < right:
+        left = 1
+        # if right == 0:
+        #     return 0
+        res = 0
+        while left <= right:
             mid = (left + right) // 2
             if good(mid):
-                left = mid + 1
+                res = mid
+                left = mid+1
             else:
-                right = mid
-        # print(mid)
-        if good(mid+1): return mid+1
-        if good(mid): return mid
-        if good(mid-1): return mid-1
-        return mid
+                right = mid-1
+
+        return res
