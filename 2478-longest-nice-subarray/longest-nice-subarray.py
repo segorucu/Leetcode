64ad663dadd8eb1abb0930194fbb7238fc6d1316
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-17 19:21:51 -0600

    Time: 79 ms (91.77%) | Memory: 33.6 MB (14.02%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.py
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -1,42 +1,15 @@
 class Solution:
 
-    def __init__(self):
-        self.counter = collections.defaultdict(list)
-        self.currmp = collections.defaultdict(int)
-
-    def check(self, i):
-        for digit in self.counter[i]:
-            if self.currmp[digit] > 0:
-                return True
-        return False
-
-    def moveright(self,i):
-        for digit in self.counter[i]:
-            self.currmp[digit] -= 1
-
-    def addright(self,i):
-        for digit in self.counter[i]:
-            self.currmp[digit] += 1
-
-    def buildmp(self,nums):
-        for i,num  in enumerate(nums):
-            for digit in range(30):
-                mask = 1 << digit
-                if (num & mask):
-                    self.counter[i].append(digit)
-
     def longestNiceSubarray(self, nums: List[int]) -> int:
         
-        self.buildmp(nums)
-    
-        n = len(nums)
+        mask = l = 0
         ans = 0
-        l = 0
-        for r in range(n):
-            while self.check(r):
-                self.moveright(l)
+        for r, num in enumerate(nums):
+            while mask & num:
+                mask ^= nums[l]
                 l += 1
-            self.addright(r)
+            mask |= num
             ans = max(ans, r-l+1)
+            
 
         return ans
