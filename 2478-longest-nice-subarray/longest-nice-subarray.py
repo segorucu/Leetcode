493b90234900537738c09e3847e5854bf9094d97
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-17 18:32:41 -0600

    Time: 3484 ms (5.18%) | Memory: 31.9 MB (71.04%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -0,0 +1,46 @@
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        
+        # counter = collections.defaultdict(list)
+
+        # for i,num  in enumerate(nums):
+        #     for digit in range(30):
+        #         mask = 1 << digit
+        #         if (num & mask):
+        #             counter[i].append(digit)
+    
+        maxdigit = 32
+        n = len(nums)
+        currmp = collections.defaultdict(int)
+
+
+        def check(num):
+            for digit in range(maxdigit):
+                mask = 1 << digit
+                if (num & mask) and currmp[digit] > 0:
+                    return True
+            return False
+
+        def moveright(num):
+            for digit in range(maxdigit):
+                mask = 1 << digit
+                if (num & mask):
+                    currmp[digit] -= 1
+
+        def addright(num):
+            for digit in range(maxdigit):
+                mask = 1 << digit
+                if num & mask:
+                    currmp[digit] += 1
+
+        ans = 0
+        l = 0
+        for r in range(n):
+            while check(nums[r]):
+                moveright(nums[l])
+                l += 1
+            addright(nums[r])
+            ans = max(ans, r-l+1)
+
+        
+        return ans
