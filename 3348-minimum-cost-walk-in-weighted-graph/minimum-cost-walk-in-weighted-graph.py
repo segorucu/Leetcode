6b29a0f928173b9a95c5ab71d29a3765aab88c9f
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-19 18:52:04 -0600

    Time: 202 ms (43.48%) | Memory: 92 MB (74.64%) - LeetSync

---
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.py
@@ -0,0 +1,50 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.size = [1] * n
+        self.count = n
+        self.cost = [(1<<32)-1 for i in range(n)]
+
+    def find(self, x):
+        # This one finds the parent and does path compression as well.
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def union(self, x, y, w):
+        px, py = self.find(x), self.find(y)
+        cost = self.cost[px] & self.cost[py] & w
+        self.cost[px] = self.cost[py] = cost
+
+        if px == py:
+            return
+        if self.size[px] < self.size[py]:
+            self.par[px] = py
+            self.size[py] += self.size[px]
+        else:
+            self.par[py] = px
+            self.size[px] += self.size[py]
+        self.count -= 1
+        
+class Solution:
+    def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        
+        union = UnionFind(n)
+        
+        for a,b,w in edges:
+            union.union(a,b,w)
+
+        ans = []
+        for a,b in query:
+            if a == b:
+                ans.append(0)
+            elif union.find(a) == union.find(b):
+                par = union.find(a)
+                ans.append(union.cost[par])
+            else:
+                ans.append(-1)
+            
+        
+        
+        return ans
+        
