f6944aa1a48f3814440dc3e1305c6dc34f5f22de
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-23 23:28:12 -0600

    Time: 18 ms (55.70%) | Memory: 85.4 MB (30.70%) - LeetSync

---
+++ 2880-find-the-closest-marked-node/find-the-closest-marked-node.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int minimumDistance(int n, vector<vector<int>>& edges, int s, vector<int>& marked) {
+        unordered_map<int,vector<tuple<int,int>>> graph;
+        vector<int> distance(n,INT_MAX);
+
+        for (const auto &edge: edges) {
+            graph[edge[0]].push_back({edge[1],edge[2]});
+        }
+
+        dijkstra(n, s, graph, distance);
+        // for (auto i = 0; i < distance.size(); ++i){
+        //     cout << i << " " << distance[i] << endl;
+        // }
+
+        int ans = INT_MAX;
+        for (auto i: marked){
+            ans = min(ans, distance[i]);
+        }
+        if (ans == INT_MAX) return -1;
+        return ans;
+    }
+
+    void dijkstra(int n, int s, unordered_map<int,vector<tuple<int,int>>> &graph, vector<int> &distance){
+        distance[s] = 0;
+        priority_queue<tuple<int, int>, vector<tuple<int, int>>, greater<>> minHeap;
+        minHeap.push({0,s});
+        unordered_set<int> visited;
+
+        while(!minHeap.empty()){
+            auto [dist, node] = minHeap.top();
+            minHeap.pop();
+            visited.insert(node);
+            for (auto [neigh, weight]: graph[node]){
+                if (!visited.count(neigh)){
+                    auto temp_dist = distance[node] + weight;
+                    if (temp_dist < distance[neigh]){
+                        distance[neigh] = temp_dist;
+                        minHeap.push({temp_dist,neigh});
+                    }
+                }
+            }
+        }
+        return;
+    }
+};
