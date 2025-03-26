e0df59735f2a394b9b3c9c7fa6c44b6aecfff1c9
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-26 16:54:33 -0600

    Time: 7 ms (80.74%) | Memory: 18.4 MB (39.76%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+
+        digits = {"0","1","2","3","4","5","6","7","8","9"}
         
         s = s.lower()
         txt = []
         lower = ord("a")
         upper = ord("z")
         for a in s:
-            if lower <= ord(a) <= upper or a in "0123456789":
+            if lower <= ord(a) <= upper or a in digits:
                 txt.append(a)
         
         n = len(txt)
