6c113cc6410a2bc412fe44e511f929e92ffca96a
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-29 22:51:52 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (54.47%) - LeetSync

---
+++ 339-nested-list-weight-sum/nested-list-weight-sum.py
@@ -0,0 +1,71 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        The result is undefined if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        The result is undefined if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+
+        def recurse(nestedList,depth):
+            sm = 0
+            # print(depth,type(nestedList))
+            if isinstance(nestedList, list):
+                for data in nestedList:
+                    sm += recurse(data,depth)
+            elif nestedList.isInteger():
+                sm = nestedList.getInteger() * depth
+            else:
+                nestedList = nestedList.getList()
+                for data in nestedList:
+                    sm += recurse(data,depth+1)
+            return sm
+
+    
+
+        sm = recurse(nestedList,1)
+        # data = nestedList[0]
+        # print(type(data))
+        # for a in data:
+        #     print(a)
+
+
+        return sm
+        
