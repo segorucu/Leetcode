97b683b3eaafe506ad64bfc446c0c2a062e4b0e7
segorucu <32857400+segorucu@users.noreply.github.com>
2025-03-29 21:59:37 -0600

    Time: 2 ms (94.58%) | Memory: 17.6 MB (85.21%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -2,27 +2,19 @@ class Solution:
     def partitionLabels(self, s: str) -> List[int]:
         
 
-        alphabet = set(s)
-        dates = defaultdict(list)
+        lastoccurence = defaultdict(int)
         for i,a in enumerate(s):
-            dates[a].append(i)
+            lastoccurence[a] = i
 
-        stack = []
-        for k,v in dates.items():
-            stack.append([v[0],v[-1]])
-
-        stack.sort()
-
-        maxdate = -1
+        size = 0
+        end = 0
         ans = []
-        beg = 0
-        for b,e in stack:
-            if b > maxdate:
-                if maxdate > -1:
-                    ans.append(maxdate-beg+1)
-                beg = b
-            maxdate = max(maxdate, e)
-        ans.append(maxdate-beg+1)
+        for i,a in enumerate(s):
+            size += 1
+            end = max(end,lastoccurence[a])
+            if i == end:
+                ans.append(size)
+                size = 0
 
         return ans
         
