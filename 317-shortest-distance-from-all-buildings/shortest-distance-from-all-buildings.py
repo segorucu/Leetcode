ed1425c7b179e4f3d0ae9daaa2a9007ad14bd464
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-01 19:54:56 -0600

    Time: 2799 ms (60.93%) | Memory: 18.4 MB (50.86%) - LeetSync

--- 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
+++ 317-shortest-distance-from-all-buildings/shortest-distance-from-all-buildings.py
@@ -4,92 +4,44 @@ class Solution:
         ones = []
         m = len(grid)
         n = len(grid[0])
-
+        zeros = []
         for r in range(m):
             for c in range(n):
                 if grid[r][c] == 1:
                     ones.append((r,c))
+                elif grid[r][c] == 0:
+                    zeros.append((r,c))
 
         def valid(r,c):
             return 0 <= r < m and 0 <= c < n
 
-        
-        def dfs(r,c):
-            neighs = [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
-            for nr,nc in neighs:
-                if valid(nr,nc):
-                    if grid[nr][nc] == 1:
-                        reachable_ones.add((nr,nc))
-                    elif grid[nr][nc] == 0 and (nr,nc) not in zeros:
-                        zeros.add((nr,nc))
-                        dfs(nr,nc)
-
-
-        r,c = ones[0]
-        reachable_ones = set()
-        reachable_ones.add((r,c))
-
-        neighs = [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
-
-        for nr, nc in neighs:
-            if valid(nr,nc) and grid[nr][nc] == 0:
-                zeros = set()
-                zeros.add((nr,nc))
-                dfs(nr,nc)
-                if len(reachable_ones) == len(ones):
-                    break
-        else:
-            return -1
-
         if len(zeros) == 0:
             return -1
 
-        def calcdist(r,c):
-            queue = deque()
-            queue.append((0,r,c))
-            visited = set()
-            visited.add((r,c))
-            sm = 0
-            while queue:
-                dist,r,c = queue.popleft()
-                if grid[r][c] == 1:
-                    sm += dist
-                    continue
-                neighs = [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
-                for nr, nc in neighs:
-                    if valid(nr,nc) and (nr,nc) not in visited and grid[nr][nc] != 2:
-                        visited.add((nr,nc))
-                        queue.append((dist+1,nr,nc))
-            return sm
-
-        medr, medc = 0, 0
+        EMPTY_SPACE = 0
+        distance_graph = defaultdict(int)
         for r,c in ones:
-            medr += r
-            medc += c
-        medr = medr // len(ones)
-        medc = medc // len(ones)
+            local_dist = inf
+            queue = deque()
+            queue.append((r,c,0))
+            while queue:
+                r, c, dist = queue.popleft()
+                neighs= [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
+                for nr, nc in neighs:
+                    if valid(nr,nc) and grid[nr][nc] == EMPTY_SPACE:
+                        grid[nr][nc] -= 1
+                        distance_graph[(nr,nc)] += dist+1
+                        queue.append((nr,nc,dist+1))
+                        local_dist = min(local_dist, distance_graph[(nr,nc)])
+            EMPTY_SPACE -= 1
+        
+        return local_dist if local_dist != inf else -1
 
-        hp = []
-        for r,c in zeros:
-            dist = abs(r-medr) + abs(c-medc)
-            heappush(hp,(dist,r,c))
 
-        mindiff = inf
-        for i in range(min(100,len(zeros))):
-            _,r,c = heappop(hp)
-            dist = calcdist(r,c)
-            mindiff = min(mindiff, dist)
-
-        return mindiff
+       
 
         
-        """
 
-[1,1,1,1,1,0]
-[0,0,0,0,0,1]
-[0,1,1,0,0,1],
-[1,0,0,1,0,1],
-[1,0,0,0,0,1]
+        return -1 if mindiff == inf else mindiff
 
-
-        """
+    
