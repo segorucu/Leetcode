4e0eab026aca5cea67dedec697bebf50078801a7
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-02 21:04:38 -0600

    Time: 3 ms (88.48%) | Memory: 18.6 MB (68.14%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -2,20 +2,40 @@ class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         
 
-        concat = firstList + secondList
-        concat.sort()
-        # print(concat)
-        n = len(concat)
-        maxdate = concat[0][1]
+        maxdate = -1
         ans = []
-        for i in range(1,n):
-            b,e = concat[i]
+        m = len(firstList)
+        n = len(secondList)
+        l1 = l2 = 0
+        while l1 < m or l2 < n:
+            if l1 < m and l2 < n:
+                if firstList[l1][0] < secondList[l2][0]:
+                    b,e = firstList[l1]
+                    l1 += 1
+                elif firstList[l1][0] > secondList[l2][0]:
+                    b,e = secondList[l2]
+                    l2 += 1
+                else:
+                    if firstList[l1][1] > secondList[l2][1]:
+                        b,e = secondList[l2]
+                        l2 += 1
+                    else:
+                        b,e = firstList[l1]
+                        l1 += 1
+            else:
+                if l1 == m:
+                    b,e = secondList[l2]
+                    l2 += 1
+                else:
+                    b,e = firstList[l1]
+                    l1 += 1
+
+           
             if b <= maxdate:
                 if ans and ans[-1][1] >= b:
                     ans[-1] = [ans[-1][0],b]
                 else:
                     ans.append([b,min(maxdate,e)])
-                # print(ans)
             maxdate = max(maxdate,e)
 
         return ans
