712f6c40b0e59e9364eecc517a9762fca4c454a9
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-06 15:57:31 -0600

    Time: 7628 ms (5.07%) | Memory: 20.4 MB (25.86%) - LeetSync

---
+++ 691-stickers-to-spell-word/stickers-to-spell-word.py
@@ -0,0 +1,36 @@
+class Solution:
+    def minStickers(self, stickers: List[str], target: str) -> int:
+        
+        vocab = set()
+        for st in stickers:
+            vocab.update(set(st))
+
+        for t in target:
+            if t not in vocab: return -1
+        n = len(stickers)
+
+        @cache
+        def dp(target):
+            if not target:
+                return 0
+
+            counter = Counter(target)
+            ops = inf
+            for i,st in enumerate(stickers):
+                counter2 = Counter(st)
+                intersection = counter & counter2
+                if not intersection:
+                    continue
+                counter -= intersection
+                txt = []
+                for k in sorted(list(counter.keys())):
+                    txt.append(counter[k]*k)
+                txt = "".join(txt)
+                if (dp(txt)+1) < ops:
+                    ops = dp(txt) + 1
+                counter += intersection
+            return ops
+
+        target = "".join(sorted(list(target)))
+        return dp(target)
+
