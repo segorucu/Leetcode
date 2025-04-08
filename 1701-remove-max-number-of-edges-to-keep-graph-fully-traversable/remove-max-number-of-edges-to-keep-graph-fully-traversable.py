474e873e3f7b92c31439482ec03884e38d31d84f
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-08 16:35:07 -0600

    Time: 255 ms (28.52%) | Memory: 61.1 MB (9.03%) - LeetSync

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.py
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.py
@@ -1,26 +1,21 @@
 class UnionFind:
     def __init__(self, n):
         self.par = [i for i in range(n)]
-        self.size = [1] * n
-        self.count = n
 
     def find(self, x):
-        # This one finds the parent and does path compression as well.
         if self.par[x] != x:
             self.par[x] = self.find(self.par[x])
         return self.par[x]
 
-    def union(self, x, y):
+    def union(self,x,y):
         px, py = self.find(x), self.find(y)
         if px == py:
             return
-        if self.size[px] < self.size[py]:
-            self.par[px] = py
-            self.size[py] += self.size[px]
+
+        if self.par[px] < self.par[py]:
+            self.par[py] = self.par[px]
         else:
-            self.par[py] = px
-            self.size[px] += self.size[py]
-        self.count -= 1
+            self.par[px] = self.par[py]
 
 class Solution:
     def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
