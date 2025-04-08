f5561915a1d5db25ebcef90f3523d3a5a906c0e5
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-07 18:30:46 -0600

    Time: 2 ms (74.36%) | Memory: 18 MB (9.05%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        
+
+        n = len(nums)
+        visited = set()
+        for i in range(n-1,-1,-1):
+            if nums[i] not in visited:
+                visited.add(nums[i])
+            else:
+                break
+        else:
+            return 0
+
+        keep = len(nums[i+1:])
+        # print(n-keep)
+        return math.ceil((n-keep)/3)
+
