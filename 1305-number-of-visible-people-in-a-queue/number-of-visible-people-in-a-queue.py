4c6f299a8147bc37244e4df97dd6b33691d48deb
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-07 18:59:45 -0600

    Time: 245 ms (5.03%) | Memory: 32.9 MB (6.48%) - LeetSync

---
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        
+        n = len(heights)
+        count = n * [0]
+        hp = []
+        for i,h in enumerate(heights):
+            while hp and hp[0][0] <= h:
+                _, ind = heappop(hp)
+                count[ind] += 1
+            if hp:
+                count[hp[0][1]] += 1
+            heappush(hp,(h,i))
+
+        return count
+
+        # i = 5
+        # count = 8
+        # hp = [11]
+        
+        
