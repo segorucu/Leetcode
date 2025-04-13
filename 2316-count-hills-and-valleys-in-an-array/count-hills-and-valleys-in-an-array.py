72a2d857c424394b09d94e3426d1ee0c4e77647e
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-13 00:33:20 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (19.74%) - LeetSync

--- 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/count-hills-and-valleys-in-an-array.py
@@ -2,17 +2,16 @@ class Solution:
     def countHillValley(self, nums: List[int]) -> int:
         
 
-        hill = 0
-        valley = 0
+        count = 0
         increasing = 0
         for l,r in pairwise(nums):
             if r > l:
                 if increasing == -1:
-                    valley += 1
+                    count += 1
                 increasing = 1
             if r < l:
                 if increasing == 1:
-                    hill += 1
+                    count += 1
                 increasing = -1
 
-        return valley + hill
+        return count
