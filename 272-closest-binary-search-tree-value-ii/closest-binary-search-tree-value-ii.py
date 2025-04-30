3568f4e9dddc43ab5cd62f7313cd7082237e25b3
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-30 15:20:41 -0600

    Time: 0 ms (100.00%) | Memory: 19.6 MB (70.72%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.py
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.py
@@ -7,26 +7,21 @@
 class Solution:
     def closestKValues(self, root: Optional[TreeNode], target: float, k: int) -> List[int]:
         
-        ans = []
+        queue = deque()
         def dfs(node):
             if not node:
                 return
-            ans.append(node.val)
+
             dfs(node.left)
+            if len(queue) < k:
+                queue.append(node.val)
+            elif abs(node.val - target) < abs(queue[0] - target):
+                queue.popleft()
+                queue.append(node.val)
+            else:
+                return
+
             dfs(node.right)
-            
+
         dfs(root)
-
-        ans.sort()
-        hp = []
-        for num in ans:
-            heappush(hp,(-abs(num-target),num))
-            if len(hp) > k:
-                heappop(hp)
-
-        ans = []
-        while hp:
-            _, val = heappop(hp)
-            ans.append(val)
-
-        return ans
+        return list(queue)
