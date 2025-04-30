8ce36bac6d1d9ad58faf7676931f09e23dd5c3ca
segorucu <32857400+segorucu@users.noreply.github.com>
2025-04-30 15:26:36 -0600

    Time: 3 ms (13.45%) | Memory: 17.9 MB (37.98%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.py
+++ 861-flipping-an-image/flipping-an-image.py
@@ -9,11 +9,11 @@ class Solution:
             r = n-1
             while l < r:
                 row[r], row[l] = row[l], row[r]
+                row[r] = 1 - row[r]
+                row[l] = 1 - row[l]
                 l += 1
                 r -= 1
-
-        for r in range(n):
-            for c in range(n):
-                image[r][c] = 1 - image[r][c]
+            if n % 2:
+                row[n//2] = 1 - row[n//2]
 
         return image
