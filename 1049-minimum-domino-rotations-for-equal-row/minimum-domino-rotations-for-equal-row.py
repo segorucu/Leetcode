821d1fb35ddb560bcb7ddaa3aa5df248daf59e1e
segorucu <32857400+segorucu@users.noreply.github.com>
2025-05-02 22:43:44 -0600

    Time: 79 ms (22.15%) | Memory: 22.8 MB (5.06%) - LeetSync

---
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        
+        occurences = defaultdict(set)
+        for i, (top,bottom) in enumerate(zip(tops,bottoms)):
+            occurences[top].add(i)
+            occurences[bottom].add(i)
+
+        options = set()
+        n = len(tops)
+        for k,v in occurences.items():
+            if len(v) == n:
+                options.add(k)
+
+        if len(options) == 0:
+            return -1
+
+        ans = n
+        for opt in options:
+            one = n
+            for top in tops:
+                if top == opt:
+                    one -= 1
+            two = n
+            for bottom in bottoms:
+                if bottom == opt:
+                    two -= 1
+            ans = min(ans,one,two)
+
+        return ans
