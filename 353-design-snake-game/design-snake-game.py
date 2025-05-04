e33f2db3d41c744dc11374ae597e8fe3e9954ea5
segorucu <32857400+segorucu@users.noreply.github.com>
2025-05-03 21:49:22 -0600

    Time: 191 ms (49.28%) | Memory: 19.4 MB (41.85%) - LeetSync

---
+++ 353-design-snake-game/design-snake-game.py
@@ -0,0 +1,49 @@
+class SnakeGame:
+
+    def __init__(self, width: int, height: int, food: List[List[int]]):
+        self.queue = deque()
+        self.queue.append((0,0))
+        self.occupied = set()
+        self.occupied.add((0,0))
+        self.cols = width
+        self.rows = height
+        self.food = deque()
+        for curr in food:
+            self.food.append(tuple(curr))
+        self.direction = {"R": [0,1], "L": [0,-1], "U": [-1,0], "D": [1,0]}
+        
+    def valid(self, r, c):
+        return 0 <= r < self.rows and 0 <= c < self.cols
+
+    def move(self, direction: str) -> int:
+        dr, dc = self.direction[direction]
+        curr = self.queue[-1]
+        nxt = (curr[0] + dr, curr[1] + dc)
+        if nxt in self.queue:
+            # print(nxt, self.occupied, self.queue)
+            if nxt != self.queue[0]:   
+                # print("h")
+                return -1
+            elif nxt == self.queue[0] and self.food and nxt == self.food[0]:  
+                # print("h")
+                return -1
+        if not self.valid(*nxt):
+            # print("h")
+            return -1
+
+        self.queue.append(nxt)
+        self.occupied.add(nxt)
+        if self.food and nxt == self.food[0]:
+            self.food.popleft()
+        else:
+            prev = self.queue.popleft()
+            if prev in self.occupied:
+                self.occupied.remove(prev)
+
+        return len(self.queue)-1
+        
+
+
+# Your SnakeGame object will be instantiated and called as such:
+# obj = SnakeGame(width, height, food)
+# param_1 = obj.move(direction)
