6639d0d74cb6b14702a0aa9afa9971b7e7ceafe4
segorucu <32857400+segorucu@users.noreply.github.com>
2025-05-07 21:12:48 -0600

    Time: 4777 ms (6.01%) | Memory: 213.4 MB (5.15%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.py
@@ -0,0 +1,34 @@
+from enum import Enum
+class Parity(Enum):
+    even = 0
+    odd = 1
+
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        
+
+        m = len(moveTime)
+        n = len(moveTime[0])
+
+        def valid(r,c):
+            return 0 <= r < m and 0 <= c < n and (r,c) not in visited
+            
+        hp = [(0,0,0,0)]
+        visited = set()
+        costmp = defaultdict(lambda: float("inf"))
+
+        while hp:
+            cost, r, c, step = heappop(hp)
+            visited.add((r,c))
+            neighs = [(r+1,c),(r-1,c),(r,c+1),(r,c-1)]
+            for nr,nc in neighs:
+                if valid(nr,nc):
+                    dist = max(moveTime[nr][nc],cost) + step + 1
+                    if dist < costmp[(nr,nc)]:
+                        heappush(hp,(dist,nr,nc,1-step))
+                        costmp[(nr,nc)] = dist
+
+        return costmp[(m-1,n-1)]
+            
+
+        return 
