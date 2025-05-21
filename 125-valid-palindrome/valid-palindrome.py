7625c285f6f62e33dc2655a31ad900068e8cb3ea
segorucu <32857400+segorucu@users.noreply.github.com>
2025-05-20 21:38:04 -0600

    Time: 7 ms (81.40%) | Memory: 20.3 MB (20.51%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,24 +1,12 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        digits = {"0","1","2","3","4","5","6","7","8","9"}
         
         s = s.lower()
+        s = s.replace(" ", "")
         txt = []
-        lower = ord("a")
-        upper = ord("z")
         for a in s:
-            if lower <= ord(a) <= upper or a in digits:
+            if a.isalnum():
                 txt.append(a)
-        
-        n = len(txt)
-        l = 0
-        r = n-1
-        while l < r:
-            if txt[l] != txt[r]:
-                return False
-            l += 1
-            r -= 1
+                
 
-
-        return True
+        return txt[:] == txt[:][::-1]
