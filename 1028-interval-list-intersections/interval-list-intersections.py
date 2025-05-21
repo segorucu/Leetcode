c5da831f2ba4097bbd57404e8fbd9e441f7b3be9
segorucu <32857400+segorucu@users.noreply.github.com>
2025-05-20 22:17:05 -0600

    Time: 3 ms (87.99%) | Memory: 18.6 MB (69.21%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.py
+++ 1028-interval-list-intersections/interval-list-intersections.py
@@ -2,42 +2,24 @@ class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         
 
-        maxdate = -1
-        ans = []
+        
+        p1 = p2 = 0
         m = len(firstList)
         n = len(secondList)
-        l1 = l2 = 0
-        while l1 < m or l2 < n:
-            if l1 < m and l2 < n:
-                if firstList[l1][0] < secondList[l2][0]:
-                    b,e = firstList[l1]
-                    l1 += 1
-                elif firstList[l1][0] > secondList[l2][0]:
-                    b,e = secondList[l2]
-                    l2 += 1
-                else:
-                    if firstList[l1][1] > secondList[l2][1]:
-                        b,e = secondList[l2]
-                        l2 += 1
-                    else:
-                        b,e = firstList[l1]
-                        l1 += 1
+        res = []
+        while p1 < m and p2 < n:
+            one = firstList[p1]
+            two = secondList[p2]
+
+            s = max(one[0],two[0])
+            e = min(one[1],two[1])
+            if s <= e:
+                res.append([s,e])
+            
+            if one[1] < two[1]:
+                p1 += 1
             else:
-                if l1 == m:
-                    b,e = secondList[l2]
-                    l2 += 1
-                else:
-                    b,e = firstList[l1]
-                    l1 += 1
-
-           
-            if b <= maxdate:
-                if ans and ans[-1][1] >= b:
-                    ans[-1] = [ans[-1][0],b]
-                else:
-                    ans.append([b,min(maxdate,e)])
-            maxdate = max(maxdate,e)
-
-        return ans
+                p2 += 1
 
 
+        return res
