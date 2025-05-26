c6169534accf1919bd4d91270b37b22a67678bf5
segorucu <32857400+segorucu@users.noreply.github.com>
2025-05-26 13:44:36 -0600

    Time: 1552 ms (88.45%) | Memory: 107.1 MB (36.03%) - LeetSync

---
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,56 @@
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+
+        
+        adj = defaultdict(list)
+        for a,b in edges:
+            adj[a].append(b)
+
+        n = len(colors)
+        colorhash = {}
+        for c in colors:
+            if c not in colorhash:
+                colorhash[c] = len(colorhash)
+        m = len(colorhash)
+
+        colormp = [m * [0] for _ in range(n)]
+
+        def dfs(node):
+            if node in path:
+                return float("inf")
+
+            if node in visit:
+                return 0
+            visit.add(node)
+            path.add(node)
+
+            colorind = colorhash[colors[node]]
+            colormp[node][colorind] = 1
+            for nei in adj[node]:
+                ret = dfs(nei)
+                if ret == float("inf"):
+                    return ret
+
+                for c, color in colorhash.items():
+                    colormp[node][color] =\
+                    max(colormp[node][color],
+                        colormp[nei][color]
+                        + (1 if color == colorind else 0))
+            path.remove(node)
+
+            return max(colormp[node])
+
+
+            path.remove(node)
+
+
+
+        res = 0
+        visit, path = set(), set()
+        for i in range(n):
+            res = max(dfs(i), res)
+            if res == float("inf"):
+                return -1
+
+        return res
+
