983d01bbb135a66b6d28e2cfe9c810acf529623c
segorucu <32857400+segorucu@users.noreply.github.com>
2025-05-29 21:53:58 -0600

    Time: 189 ms (63.35%) | Memory: 49.2 MB (42.08%) - LeetSync

---
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.py
@@ -0,0 +1,40 @@
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        
+        if node1 == node2:
+            return node1
+        
+        def traverse(node):
+            distance= 0
+            graph = {}
+            while node != -1:
+                if node in graph:
+                    return graph
+                graph[node] = distance
+                node = edges[node]
+                distance += 1
+            return graph
+
+        graph1 = traverse(node1)
+        graph2 = traverse(node2)
+        print(graph1,graph2)
+
+        common = graph1.keys() & graph2.keys()
+        if not common:
+            return -1
+
+        maxdist = float("inf")
+        selected = 10**6
+        for key in common:
+            dist = max(graph1[key], graph2[key])
+            # print(key, dist, maxdist)
+            if dist < maxdist:
+                maxdist = dist
+                selected = key
+            elif dist == maxdist:
+                selected = min(selected, key)
+
+        return selected
+
+        # 5 -> 8 -> 1 -> 4 -> 9 -> 1
+        # 6 -> 4 -> 9 -> 1
