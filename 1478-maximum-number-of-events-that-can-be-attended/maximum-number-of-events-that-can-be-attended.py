097f8382dee210e21b283d93ae60f5a2ea2eac82
segorucu <32857400+segorucu@users.noreply.github.com>
2025-07-06 20:10:51 -0600

    Time: 191 ms (17.99%) | Memory: 53.2 MB (55.74%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -1,28 +1,26 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
         
-        events.sort(key = lambda x: (x[1], x[0]))
+        events.sort()
         maxval = 0
         minval = inf
         for event in events:
             maxval = max(maxval, event[1])
             minval = min(minval, event[0])
-        available = SortedList()
-        for date in range(minval, maxval+1):
-            available.add(date)
-
+        
+        pq = []
         cnt = 0
-        for event in events:
-            start, end = event
-            if not available:
-                break
-            ind = available.bisect_left(start)
-            # print(start, end, ind, available)
-            if ind == 0 and start <= available[0] <= end:
-                available.pop(0)
+        j = 0
+        for date in range(minval, maxval+1):
+            while j < len(events) and events[j][0] == date:
+                heappush(pq,events[j][1])
+                j += 1
+
+            while pq and pq[0] < date:
+                heappop(pq)
+
+            if pq and pq[0] >= date:
+                heappop(pq)
                 cnt += 1
-            elif ind < len(available) and available[ind] <= end:
-                available.remove(available[ind])
-                cnt += 1
-            
+
         return cnt
