ac05b89b565ce146af2f41598fa2f4b5881579a0
segorucu <32857400+segorucu@users.noreply.github.com>
2025-07-08 19:21:44 -0600

    Time: 95 ms (13.69%) | Memory: 35.9 MB (87.45%) - LeetSync

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxFreeTime(self, eventTime: int, k: int, startTime: List[int], endTime: List[int]) -> int:
+        
+        startTime = [0] + startTime + [eventTime]
+        endTime = [0] + endTime + [eventTime]
+
+        l = 0
+        n = len(startTime)
+        meeting_time = 0
+        ans = 0
+        for r in range(1,n-1):
+            meeting_time += endTime[r] - startTime[r]
+            if r < n -1:
+                startr = startTime[r+1]
+            else:
+                startr = eventTime
+            while r - l >= k:
+                meeting_time = meeting_time - (endTime[l] - startTime[l])
+                l += 1
+            endl = endTime[l-1]
+            freetime = startr - endl - meeting_time
+            ans = max(ans, freetime)
+
+        return ans
+
+        # [0,1,3,5]
+        # [0,2,5,5]
+            
