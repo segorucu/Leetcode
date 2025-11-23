e1c065b1c80d80216c4be7da665b2a80c4a41472
segorucu <32857400+segorucu@users.noreply.github.com>
2025-11-23 12:56:40 -0800

    Time: 15 ms (83.87%) | Memory: 21.7 MB (66.57%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
@@ -1,49 +1,38 @@
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
 
+        
+
+        sm = sum(nums)
+        if sm % 3 == 0:
+            return sm
+
         nums.sort()
-        sm = 0
-        n = len(nums)
         ones = []
         twos = []
         for num in nums:
-            if num % 3 == 0:
-                sm += num
-            elif num % 3 == 1:
+            if num % 3 == 1 and len(ones) < 3:
                 ones.append(num)
-            elif num % 3 == 2:
+            elif num % 3 == 2 and len(twos) < 3:
                 twos.append(num)
-
-        while len(ones) > 3 and len(twos) > 3:
-            sm += ones.pop()
-            sm += twos.pop()
-
-        while len(ones) > 5:
-            sm += ones.pop()
-            sm += ones.pop()
-            sm += ones.pop()
-
-        while len(twos) > 5:
-            sm += twos.pop()
-            sm += twos.pop()
-            sm += twos.pop()
-
-
-        remainder = [0]
-        arr = ones + twos
-        def backtrack(i, currsm):
-            if i == len(arr):
-                if currsm % 3 == 0:
-                    remainder[0] = max(remainder[0], currsm)
-                return
-
-            backtrack(i+1, currsm+arr[i])
-            backtrack(i+1, currsm)
-
         
-        backtrack(0, 0)
+        if sm % 3 == 1:
+            one = sm
+            if ones:
+                one = min(one, ones[0])
+            if len(twos) > 1:
+                one = min(one, sum(twos[0:2]))
+            return sm - one
+        elif sm % 3 == 2:
+            two = sm
+            if len(ones) > 1:
+                two = min(two, sum(ones[0:2]))
+            if twos:
+                two = min(two, twos[0])
+            return sm - two
 
-        return sm + remainder[0]
+
+
 
         
 
