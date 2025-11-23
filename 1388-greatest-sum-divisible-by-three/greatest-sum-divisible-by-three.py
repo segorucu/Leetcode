04ec8404520e1de2b4ac18403680615566058589
segorucu <32857400+segorucu@users.noreply.github.com>
2025-11-23 12:05:07 -0800

    Time: 51 ms (56.89%) | Memory: 22.3 MB (24.93%) - LeetSync

---
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.py
@@ -0,0 +1,53 @@
+class Solution:
+    def maxSumDivThree(self, nums: List[int]) -> int:
+
+        nums.sort()
+        sm = 0
+        n = len(nums)
+        ones = []
+        twos = []
+        for num in nums:
+            if num % 3 == 0:
+                sm += num
+            elif num % 3 == 1:
+                ones.append(num)
+            elif num % 3 == 2:
+                twos.append(num)
+        print(ones)
+        print(twos)
+
+        while len(ones) > 3 and len(twos) > 3:
+            sm += ones.pop()
+            sm += twos.pop()
+
+        while len(ones) > 5:
+            sm += ones.pop()
+            sm += ones.pop()
+            sm += ones.pop()
+
+        while len(twos) > 5:
+            sm += twos.pop()
+            sm += twos.pop()
+            sm += twos.pop()
+
+
+        remainder = [0]
+        arr = ones + twos
+        print(arr)
+        def backtrack(i, currsm):
+            if i == len(arr):
+                if currsm % 3 == 0:
+                    remainder[0] = max(remainder[0], currsm)
+                return
+
+            backtrack(i+1, currsm+arr[i])
+            backtrack(i+1, currsm)
+
+        
+        backtrack(0, 0)
+
+        return sm + remainder[0]
+
+        
+
+        
