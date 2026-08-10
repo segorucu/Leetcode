231620b76d5f929c88f38718de8723cf931cdaee
segorucu <32857400+segorucu@users.noreply.github.com>
2026-08-09 20:07:12 -0700

    Time: 0 ms (100.00%) | Memory: 20.2 MB (5.93%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        
+        n = len(nums)
+
+        @cache
+        def dp(turn,l,r):
+            if l == r:
+                if turn % 2 == 0:
+                    return nums[l]
+                else:
+                    return 0
+
+            if turn % 2 == 0:
+                res = 0
+                return max(dp(turn+1,l+1,r) + nums[l], dp(turn+1,l,r-1) + nums[r])
+            else:
+                return min(dp(turn+1,l+1,r), dp(turn+1,l, r-1))
+            
+
+        res = dp(0,0,n-1)
+        return res >= sum(nums) / 2
+
+
+
+
+        
